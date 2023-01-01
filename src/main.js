38a61ed58363c30987f7179ce0f2e58fb4584cd8
Latrevion <xiaopikaqiu@outlook.com>
2023-01-01 21:46:58 +0800

    完善main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './permission.js'
+import './assets/common/fonts/iconfont.css'
+import store from './store/index.js'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+// 挂载到Vue实力上，全局可通过this.$store进行调用
+Vue.prototype.$store = store
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
